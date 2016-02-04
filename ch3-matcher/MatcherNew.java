2dc3ecfe0736cd9687122890c3bb801a1986ce6d
lynnlangit <lynnlangit@live.com>
2016-02-03 18:33:07 -0800

    first refactoring - MatcherNew

---
+++ ch3-matcher/MatcherNew.java
@@ -0,0 +1,17 @@
+public class MatcherNew {
+    public MatcherNew() {}
+   
+    public boolean match(int[] expected, int[] actual, 
+        int clipLimit, int delta) 
+    {
+       for (int i = 0; actual.length; i++)
+       {
+           if (actual[i] > cliplimit)
+           actual[i] = cliplimit
+           
+           if (acutal.length != expected.length) || (Math.abs(expected[i] - acutal[i] > delta))
+           return false;
+       }
+       return true;
+    }
+}
